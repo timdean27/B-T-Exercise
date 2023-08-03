3dbc628744eff1cfbf0352413b400b6a7dcb096b
timdean27 <timdean27@yahoo.com>
2023-08-02 23:41:12 -0400

    Working on displaying videos

--- bt_exercise/src/Home.js
+++ bt_exercise/src/Home.js
@@ -1,11 +1,20 @@
-import React from 'react';
-import Header from './Components/Header';
+// Home.js
+
+import React, { useContext } from "react";
+import Header from "./Components/Header";
+import VideoViewer from "./Pages/VideoViewer";
+import BackButton from "./Components/BackButton";
+
 
 const Home = ({ currentUser }) => {
   return (
     <div>
       <Header currentUser={currentUser} />
-      Home
+      <BackButton />
+      <h2>Welcome to the Home Page</h2>
+      <div className="video-slider">
+        <VideoViewer />
+      </div>
     </div>
   );
 };
